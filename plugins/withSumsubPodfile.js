5c5667b84fae72152c1490fadcf0c56530d32696
nonyonah <nonyonah@gmail.com>
2026-01-17 14:23:15 +0100

    fix(kyc): Use correct Sumsub specs repo for CocoaPods
    Changed from wrong repo to: https://github.com/SumSubstance/Specs.git
    This repo contains the IdensicMobileSDK podspec

--- plugins/withSumsubPodfile.js
+++ plugins/withSumsubPodfile.js
@@ -4,7 +4,7 @@ const path = require('path');
 
 /**
  * Config plugin to add Sumsub's CocoaPods source to the Podfile
- * This is required because the IdensicMobileSDK pod is in a private repo
+ * This is required because the IdensicMobileSDK pod is in Sumsub's private specs repo
  */
 const withSumsubPodfile = (config) => {
     return withDangerousMod(config, [
@@ -16,16 +16,17 @@ const withSumsubPodfile = (config) => {
                 let podfileContent = fs.readFileSync(podfilePath, 'utf8');
 
                 // Check if Sumsub source is already added
-                if (!podfileContent.includes('source \'https://cdn.cocoapods.org/\'')) {
+                if (!podfileContent.includes('SumSubstance/Specs')) {
                     // Add CocoaPods sources at the beginning of the Podfile
-                    const sourcesBlock = `# CocoaPods sources
+                    // IMPORTANT: Sumsub's specs repo must come BEFORE cdn.cocoapods.org
+                    const sourcesBlock = `# CocoaPods sources for Sumsub SDK
+source 'https://github.com/SumSubstance/Specs.git'
 source 'https://cdn.cocoapods.org/'
-source 'https://github.com/nicklockwood/SwiftFormat'
 
 `;
                     podfileContent = sourcesBlock + podfileContent;
                     fs.writeFileSync(podfilePath, podfileContent);
-                    console.log('[Sumsub] Added CocoaPods source to Podfile');
+                    console.log('[Sumsub] Added SumSubstance/Specs CocoaPods source to Podfile');
                 }
             }
 
